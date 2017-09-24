84138d139b8840b02ecabbbf0ab273a3a21b9ef3
gabrieldaarte <32078877+gabrieldaarte@users.noreply.github.com>
2017-09-24 01:12:56 -0300

    Update numero primo - FINAL.c

--- numero primo - FINAL.c
+++ numero primo - FINAL.c
@@ -42,7 +42,7 @@ int main()
 		scanf("%d", &p);
 	}
 	
-	if (p=2) // Exibe mensagem de agradecimento, caso opte por não continuar.
+	if (p=2) //Exibe mensagem de agradecimento, caso opte por não continuar (opção 2).
 	
 	printf("\nObrigado, ate a proxima! Para ver o meu codigo entre no link:\n\nhttps://goo.gl/cH5YiE\n\n");
 
